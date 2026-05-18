def31602ce29534f8e076a2026b831963692d358
smyaltrigoyolu <bilgisayaranahesap@gmail.com>
2026-05-19 02:39:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const questions = {
         },
         {
             question: "Dik koordinat düzleminde pozitif yönlü 270°lik merkez açı, orijin merkezli birim çemberi A noktasında kesmektedir. Buna göre A noktasının koordinatlarını bulunuz.",
-            options: ["(1,1)", "(0,1)", "(0,-1)", "(-1,0)"],
+            options: ["1,1", "0,1", "0,-1", "-1,0"],
             correct: 2
         },
         {
@@ -103,7 +103,7 @@ const questions = {
         },
         {
             question: "cos(x) = 1/2 denkleminin çözüm kümesi nedir?",
-            options: ["x = π/3 + 2kπ veya x = 5π/3 + 2kπ", "x = π/6 + 2kπ veya x = 11π/6 + 2kπ", "x = π/4 + 2kπ veya x = 7π/4 + 2kπ", "x = π/2 + 2kπ veya x = 3π/2 + 2kπ"],
+            options: ["x = π/3 + 2kπ veya x = 5π/3 + 2kπ", "x = π/6 + 2kπ veya x = 11π/6 + 2kπ", "x = π/4 + kπ veya x = 7π/4 + kπ", "x = π/2 + kπ veya x = 3π/2 + kπ"],
             correct: 1
         },
         {
@@ -148,15 +148,257 @@ const questions = {
         }
     ],
     medium: [
-        // Orta sorular buraya eklenecek
+        {
+            question: "(tan210° - sin300°) / (cos360° - cos120°) ifadesinin değeri kaçtır?",
+            options: ["√3/2", "√3/6", "1/2", "5√3/9"],
+            correct: 3
+        },
+        {
+            question: "(cos2π - sinπ) / (cot(π/4) + tan2π) ifadesinin değeri kaçtır?",
+            options: ["-1", "0", "-2", "1"],
+            correct: 3
+        },
+        {
+            question: "arcsin(√3/2) ifadesinin eşiti aşağıdakilerden hangisidir?",
+            options: ["π/3", "π/6", "2π/3", "π/2"],
+            correct: 0
+        },
+        {
+            question: "arctan1 ifadesinin eşiti aşağıdakilerden hangisidir?",
+            options: ["5π/4", "π/4", "π/6", "π/3"],
+            correct: 1
+        },
+        {
+            question: "(tan50° / cot40°) + (sin20° / cos250°) işleminin sonucu kaçtır?",
+            options: ["2", "1", "-1", "0"],
+            correct: 3
+        },
+        {
+            question: "sin²(2π - x) . tan²(π/2 + x) ifadesinin eşiti aşağıdakilerden hangisidir?",
+            options: ["tan²x", "cos²x", "1", "sin²x"],
+            correct: 1
+        },
+        {
+            question: "7x = π olduğuna göre, (cos3x / cos4x) - (sin5x / sin2x) ifadesinin değeri kaçtır?",
+            options: ["2", "0", "-2", "1"],
+            correct: 2
+        },
+        {
+            question: "f(x) = 5 . sin⁴(3x - 1) fonksiyonunun esas periyodu hangisidir?",
+            options: ["π/2", "π/3", "π/6", "2π/3"],
+            correct: 1
+        },
+        {
+            question: "tan(π - arcsin(1/1/5)) ifadesinin değeri kaçtır?",
+            options: ["-1/2", "1/2", "2", "-2"],
+            correct: 0
+        },
+        {
+            question: "tanx - cotx = 5 olduğuna göre, tan²x + cot²x kaçtır?",
+            options: ["26", "27", "25", "22"],
+            correct: 1
+        },
+        {
+            question: "cos60° + cos61° + cos62° + ... + cos119° ifadesinin değeri kaçtır?",
+            options: ["-1", "-1/2", "1", "1/2"],
+            correct: 3
+        },
+        {
+            question: "ifadesine göre; (2sinx - cosx) / (2cosx + sinx) = 1/3 olduğuna göre, cotx kaçtır?",
+            options: ["1", "2", "1/2", "1/3"],
+            correct: 0
+        },
+        {
+            question: "cos120° / tan330° ifadesinin değeri kaçtır?",
+            options: ["1/2", "√3/2", "-√3/2", "-1/2"],
+            correct: 1
+        },
+        {
+            question: "3π/2 < x < 2π olmak üzere sinx = -7/25 olduğuna göre, secx kaçtır?",
+            options: ["25/24", "-25/24", "-24/25", "24/25"],
+            correct: 0
+        },
+        {
+            question: "8sinx - 2 ifadesinin alabileceği en büyük değer kaçtır?",
+            options: ["3", "6", "4", "5"],
+            correct: 1
+        },
+        {
+            question: "sin²x / (1 - cosx) ifadesi aşağıdakilerden hangisine eşittir?",
+            options: ["sinx - cosx", "1 + sinx", "1 + cosx", "1 - sinx"],
+            correct: 2
+        },
+        {
+            question: "sin²(π/8) + cos²(π/8) ifadesinin değeri kaçtır?",
+            options: ["2", "1", "1/2", "1/3"],
+            correct: 1
+        },
+        {
+            question: "(3sin²50° + 3cos²50° - 2) / (4sin²40° + 4cos²40°) ifadesinin değeri kaçtır?",
+            options: ["1/5", "1/4", "1/3", "1/2"],
+            correct: 1
+        },
+        {
+            question: "sinx - cosx = 1/5 olduğuna göre, sinx . cosx ifadesinin değeri kaçtır?",
+            options: ["2", "12/25", "3/5", "18/25"],
+            correct: 1
+        },
+        {
+            question: "(sinx - cosx) / (2sinx + 4cosx) = 1/3 olduğuna göre, cotx kaçtır?",
+            options: ["1/8", "1/7", "1/6", "1/3"],
+            correct: 1
+        },
+        {
+            question: "(sin²20° + sin²70°) / (tan25° . tan65°) ifadesinin değeri kaçtır?",
+            options: ["1", "2", "3", "4"],
+            correct: 0
+        },
+        {
+            question: "ABC bir üçgen olmak üzere, cos(A + B) + cosC ifadesi aşağıdakilerden hangisine eşittir?",
+            options: ["0", "1", "2cosC", "-1"],
+            correct: 0
+        },
+        {
+            question: "x = π/10 olmak üzere, (sin3x . cos6x) / (cos4x . sin7x) ifadesinin değeri kaçtır?",
+            options: ["2", "1", "0", "-1"],
+            correct: 3
+        },
+        {
+            question: "cos(π/4) - sin(9π/4) + cot(17π/4) ifadesinin değeri kaçtır?",
+            options: ["2", "1", "0", "-1"],
+            correct: 1
+        },
+        {
+            question: "f(x) = sin4x + tan6x olduğuna göre, f(π/24) değeri kaçtır?",
+            options: ["1/4", "1/2", "1", "3/2"],
+            correct: 3
+        },
+        {
+            question: "cot(-θ) / tan(-θ) ifadesi aşağıdakilerden hangisine eşittir?",
+            options: ["cot²θ", "-cotθ", "tan²θ", "-cot²θ"],
+            correct: 0
+        },
+        {
+            question: "(sin(-300°) + cos(-150°)) / tan(-60°) ifadesinin değeri kaçtır?",
+            options: ["2", "1", "0", "-1"],
+            correct: 3
+        },
+        {
+            question: "arctan(√3) ifadesinin değeri kaçtır?",
+            options: ["π/8", "π/6", "π/4", "π/3"],
+            correct: 3
+        }
     ],
     hard: [
-        // Zor sorular buraya eklenecek
+        {
+            question: "cos[π/2 - arcsin(3/5)] ifadesinin değeri kaçtır?",
+            options: ["1", "4/5", "3/5", "2/5"],
+            correct: 2
+        },
+        {
+            question: "Bir ABC üçgeninin kenarları arasında c² = a² + b² + a.b bağıntısı vardır. Buna göre, m(Ĉ) kaç derecedir?",
+            options: ["100", "110", "120", "135"],
+            correct: 2
+        },
+        {
+            question: "cos(π/2 + arccot2) ifadesinin değeri kaçtır?",
+            options: ["-1/√5", "1/√5", "2/√5", "-2/√5"],
+            correct: 0
+        },
+        {
+            question: "Şekildeki ABC üçgeninde m(BÂC) = 45°, |AB| = 40 cm ve |AC| = 30√2 cm olduğuna göre, |BC| = x kaç cm'dir?",
+            image: "images/zor4.png",
+            options: ["10√10", "30", "10√11", "20"],
+            correct: 0
+        },
+        {
+            question: "Şekildeki ABC üçgeninde |AC| = 20 cm, |BC| = 60 cm ve m(BÂC) = x + 90° olduğuna göre, sinx kaçtır?",
+            image: "images/zor5.png",
+            options: ["1/√10", "1/√5", "1/3", "1/2"],
+            correct: 0
+        },
+        {
+            question: "sin42° . cos18° + cos42° . sin18° ifadesinin değeri kaçtır?",
+            options: ["√3/2", "1/2", "1/3", "√3/4"],
+            correct: 0
+        },
+        {
+            question: "sin105° ifadesinin değeri kaçtır?",
+            options: ["(√6 + √2) / 4", "(√6 + √2) / 5", "(√6 + √2) / 6", "(√6 - √2) / 4"],
+            correct: 0
+        },
+        {
+            question: "(tan40° + tan20°) / (1 - tan40° . tan20°) ifadesinin değeri kaçtır?",
+            options: ["1", "√2", "√3", "2"],
+            correct: 2
+        },
+        {
+            question: "tanx = 1/3 olduğuna göre, sin2x kaçtır?",
+            options: ["1/10", "1/5", "2/5", "3/5"],
+            correct: 3
+        },
+        {
+            question: "(cosx + 5) . (7 - cosx) ifadesinin en büyük değeri kaçtır?",
+            options: ["40", "36", "35", "28"],
+            correct: 1
+        },
+        {
+            question: "2 . sin22,5° . cos22,5° ifadesinin değeri kaçtır?",
+            options: ["1/|√2", "√3/2", "1/2", "1"],
+            correct: 0
+        },
+        {
+            question: "3 - 2sin²x = √3 olduğuna göre, cos2x kaçtır?",
+            options: ["√3 - 2", "√3 - 1", "2√3 / 3", "0"],
+            correct: 0
+        },
+        {
+            question: "sin²6° + 5 . tan20° . tan70° + sin²84° ifadesinin değeri kaçtır?",
+            options: ["4", "5", "6", "7"],
+            correct: 2
+        },
+        {
+            question: "a, b ve c dar açılar olmak üzere cosa = 1/4, cosb = 1/2, cosc = 1/3 olduğuna göre, aşağıdakilerden hangisi doğrudur?",
+            options: ["a > b > c", "a > c > b", "b > c > a", "c > b > a"],
+            correct: 1
+        },
+        {
+            question: "sin60° . tan30° - √2 . cos45° ifadesinin değeri kaçtır?",
+            options: ["-1/2", "1/2", "3/2", "1"],
+            correct: 0
+        }
     ]
 };
 
-// Kutu oyunu açıklaması - Kullanıcı tarafından doldurulacak
-const gameDescription = "Kutu oyunu açıklaması buraya gelecek. Lütfen bu metni güncelleyin.";
+// Kutu oyunu açıklaması - Trigo Yolu Kuralları ve Oynanışı
+const gameDescription = {
+    name: "Trigo Yolu",
+    howToPlay: {
+        title: "Nasıl Oynanır?",
+        imageHtml: `<div class="console-icon" style="text-align:center; margin: 10px 0;"><span style="font-size: 48px;">🎮</span></div>`,
+        content: [
+            "İlk Hamle: Oyuna kimin başlayacağı rastgele bir yöntemle belirlenir.",
+            "Kaderini Belirleyen Renkler: Sırası gelen oyuncu zarı atar. Zarın üzerinde gelen renk, çözmeniz gereken sorunun zorluk derecesini belirler.",
+            "Dijital Erişim: Oyun kutusunun üzerindeki QR kodu okutarak oyunun web sitesine giriş yapın. Sitede, zarda gelen renge uygun kategoriyi seçerek sorunuzu görüntüleyin.",
+            "Zamana Karşı Yarış: Soruları çözmek için süreniz kısıtlıdır! (Kolay Seviye: 30 Saniye, Orta Seviye: 45 Saniye, Zor Seviye: 60 Saniye)",
+            "Hamle Hakkını Kazan: Soruyu doğru cevaplarsanız bir hamle hakkı kazanırsınız. Bu hakkınızı piyonunuzu ilerletmek veya rakibinizin yoluna engel koymak için kullanabilirsiniz. Yanlış cevapta sıra rakibe geçer.",
+            "Şansın Gücü (Yeşil Bölüm): Eğer zarın yeşil yüzü gelirse şanslısınız! 'Şans Bölümü'ne ulaştınız demektir. Hemen bir Şans Kartı çekin ve üzerindeki talimatı uygulayın.",
+            "Zafere Giden Yol: Kendi piyonunu karşı taraftaki son sıraya ulaştıran ilk oyuncu, matematik savaşını kazanmış sayılır!"
+        ]
+    },
+    rules: {
+        title: "Kurallar",
+        content: [
+            "Başlangıç Pozisyonu: Her iki oyuncu da piyonunu, kendisine en yakın olan ilk sıranın tam orta karesine yerleştirerek oyuna başlar. İlk oyuncu rastgele seçilir.",
+            "Hamle Seçimi: Soruyu doğru cevaplayan oyuncu şu iki hareketten yalnızca birini yapabilir: Piyonunu yatay veya dikey olarak bir kare ilerletmek YA DA Rakibinin ilerleyişini durdurmak için oyun alanına bir engel yerleştirmek.",
+            "Hareket Kısıtlamaları: Piyonlar çapraz hareket edemez. Sadece ileri, geri, sağa ve sola gidebilirler.",
+            "Atlama Kuralı: İki piyon karşı karşıya gelirse, sırası gelen oyuncu rakibinin piyonunun üzerinden atlayarak bir kare öne geçebilir.",
+            "Stratejik Engel Kullanımı: Her oyuncunun toplamda 10 adet engel hakkı vardır. Engeller bittiğinde sadece piyon hareket ettirilebilir.",
+            "Zorunlu Geçiş Yolu: Rakibinizin yolunu tamamen kapatmak yasaktır. Hedef sıraya ulaşabilmesi için her zaman en az bir adet açık kare bırakılmalıdır.",
+            "Hata ve Kayıp: Sorunun yanlış cevaplanması veya sürenin dolması durumunda hamle hakkı doğrudan rakibe geçer."
+        ]
+    }
+};
 
 // DOM elementleri
 const mainScreen = document.getElementById('mainScreen');
@@ -181,7 +423,7 @@ let shuffledCorrectIndex = null;
 
 // Timer süreleri zorluk bazlı
 const timerDurations = {
-    easy: 30,
+    easy: 10,
     medium: 45,
     hard: 60
 };
@@ -218,24 +460,18 @@ backBtn.addEventListener('click', () => {
 
 // Soru başlat
 function startQuestion() {
-    // Soruları kontrol et
     if (questions[currentDifficulty].length === 0) {
         alert('Bu zorluk seviyesinde henüz soru yok!');
         return;
     }
     
-    // Rastgele soru seç
     const randomIndex = Math.floor(Math.random() * questions[currentDifficulty].length);
     currentQuestion = questions[currentDifficulty][randomIndex];
     
-    // Ekranı değiştir
     mainScreen.classList.add('hidden');
     questionScreen.classList.remove('hidden');
     
-    // Soruyu göster
     displayQuestion();
-    
-    // Timer'ı başlat
     startTimer();
 }
 
@@ -243,7 +479,6 @@ function startQuestion() {
 function displayQuestion() {
     questionTextEl.textContent = currentQuestion.question;
 
-    // Görsel varsa göster
     if (currentQuestion.image) {
         questionImageEl.innerHTML = `<img src="${currentQuestion.image}" alt="Soru görseli">`;
         questionImageEl.classList.remove('hidden');
@@ -251,27 +486,22 @@ function displayQuestion() {
         questionImageEl.classList.add('hidden');
     }
 
-    // Seçenekleri karıştır
     const shuffledOptions = [...currentQuestion.options];
     const originalCorrectIndex = currentQuestion.correct;
     
-    // Shuffle array using Fisher-Yates algorithm
     for (let i = shuffledOptions.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [shuffledOptions[i], shuffledOptions[j]] = [shuffledOptions[j], shuffledOptions[i]];
     }
     
-    // Find the new index of the correct answer
     shuffledCorrectIndex = shuffledOptions.indexOf(currentQuestion.options[originalCorrectIndex]);
 
-    // Seçenekleri göster
     optionBtns.forEach((btn, index) => {
         btn.textContent = shuffledOptions[index];
         btn.classList.remove('correct', 'wrong');
         btn.disabled = false;
     });
 
-    // Sonuç mesajını gizle
     resultMessageEl.classList.add('hidden');
 }
 
@@ -285,7 +515,6 @@ function startTimer() {
         timeLeft--;
         timerEl.textContent = timeLeft;
 
-        // Renk değişimi: yeşilden kırmızıya
         if (timeLeft <= 10) {
             timerEl.classList.remove('warning');
             timerEl.classList.add('critical');
@@ -294,7 +523,6 @@ function startTimer() {
             timerEl.classList.add('warning');
         }
 
-        // Süre doldu
         if (timeLeft <= 0) {
             clearInterval(currentTimer);
             handleTimeout();
@@ -304,17 +532,14 @@ function startTimer() {
 
 // Süre dolduğunda
 function handleTimeout() {
-    // Tüm butonları devre dışı bırak
     optionBtns.forEach(btn => {
         btn.disabled = true;
     });
     
-    // Yanlış mesajı göster
     resultMessageEl.textContent = 'HAMLE HAKKI RAKİBE GEÇTİ';
     resultMessageEl.classList.remove('hidden', 'success');
     resultMessageEl.classList.add('failure');
     
-    // 3 saniye sonra ana ekrana dön
     setTimeout(() => {
         returnToMainScreen();
     }, 3000);
@@ -323,32 +548,25 @@ function handleTimeout() {
 // Seçenek butonlarına tıklama olayı
 optionBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        // Timer'ı durdur
         clearInterval(currentTimer);
 
-        // Tüm butonları devre dışı bırak
         optionBtns.forEach(b => {
             b.disabled = true;
         });
 
-        // Cevabı kontrol et
         if (index === shuffledCorrectIndex) {
-            // Doğru cevap
             btn.classList.add('correct');
             resultMessageEl.textContent = 'HAMLE HAKKI KAZANDINIZ';
             resultMessageEl.classList.remove('hidden', 'failure');
             resultMessageEl.classList.add('success');
         } else {
-            // Yanlış cevap
             btn.classList.add('wrong');
-            // Doğru cevabı göster
             optionBtns[shuffledCorrectIndex].classList.add('correct');
             resultMessageEl.textContent = 'HAMLE HAKKI RAKİBE GEÇTİ';
             resultMessageEl.classList.remove('hidden', 'success');
             resultMessageEl.classList.add('failure');
         }
 
-        // 3 saniye sonra ana ekrana dön
         setTimeout(() => {
             returnToMainScreen();
         }, 3000);
@@ -360,14 +578,18 @@ function returnToMainScreen() {
     questionScreen.classList.add('hidden');
     mainScreen.classList.remove('hidden');
     
-    // Timer'ı temizle
     if (currentTimer) {
         clearInterval(currentTimer);
         currentTimer = null;
     }
     
-    // Değişkenleri sıfırla
     currentQuestion = null;
     currentDifficulty = null;
     timeLeft = 60;
 }
+
+document.querySelectorAll('.global-back-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        returnToMainScreen();
+    });
+});
